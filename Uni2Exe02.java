6efd9362420a5b3d35389bad801d3b5fa70ff0f2
Guilherme Babel Machado <93015535+Babelg@users.noreply.github.com>
2025-03-05 13:18:48 -0300

    Uni2Exe02.java

--- Uni2Exe02.java
+++ Uni2Exe02.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class Uni2Exe02 {
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
 
-    
+        // Entrada
         System.out.print("Digite o primeiro número: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Digite o segundo número: ");
         double num2 = scanner.nextDouble();
 
-    
+        // Cálculo
         double resultado = num1 * num2;
 
-        
-        System.out.println("O resultado da multiplicação é: " + resultado);
+        // Resultado
+        System.out.printf("O resultado da multiplicação é: %.2f%n", resultado);
 
-       
+        // Scanner
         scanner.close();
     }
 }
