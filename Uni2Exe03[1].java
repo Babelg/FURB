57f6d83af9867555200d99f0f060d04b6356478e
Guilherme Babel Machado <93015535+Babelg@users.noreply.github.com>
2025-03-05 13:21:30 -0300

    Uni2Exe03[1].java

--- Uni2Exe03[1].java
+++ Uni2Exe03[1].java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class AreaCirculo {
     public static void main(String[] args) {
-        //PI
-        double pi = 3.14159;
-        
-        // Scanner 
+        // Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // raio
+        // Entrada
         System.out.print("Digite o valor do raio: ");
         double raio = scanner.nextDouble();
         
-        //área
-        double area = pi * Math.pow(raio, 2);
+        // Cálculo
+        double area = Math.PI * Math.pow(raio, 2);
         
-        //resultado
-        System.out.printf("A área do círculo é: %.4f\n", area);
-        
-       
+        // Resultado
+        System.out.printf("A área do círculo é: %.4f%n", area);
+
+        //Scanner
+        scanner.close();
     }
 }
