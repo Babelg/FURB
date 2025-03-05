33a97487cc2cc2c82ab637301e19564c0786c1c8
Guilherme Babel Machado <93015535+Babelg@users.noreply.github.com>
2025-03-05 13:11:42 -0300

    Uni2Exe01.java

--- App.java
+++ App.java
@@ -1,18 +1,19 @@
-
-
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int valor1= 1;
-        int valor2=6;
-        int total= valor1+valor2;
-        System.out.println("O resultado é:"+ (total)); 
-       int nota1=10;
-       int nota2=8;
-       int nota3= 7;
-    System.out.println("A média é:" + (nota1+nota2+nota3)/3);
-   
 
+        //Soma
+        int valor1 = 1;
+        int valor2 = 6;
+        int total = valor1 + valor2;
+        System.out.println("O resultado da soma é: " + total);
+
+        //Média
+        double nota1 = 10;
+        double nota2 = 8;
+        double nota3 = 7;
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média é: " + media);
     }
 }
-
