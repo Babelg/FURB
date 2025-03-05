3a61510438ef4aec9867921a0df08d24f144c97e
Guilherme Babel Machado <93015535+Babelg@users.noreply.github.com>
2025-03-05 13:24:27 -0300

    Uni2Exe03.java

--- Uni2Exe03.java
+++ Uni2Exe03.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class Uni2Exe03 {
     public static void main(String[] args) {
-        
-        double pi = 3.14159;
-        
-        
         Scanner scanner = new Scanner(System.in);
 
-        
+        // Raio
         System.out.print("Digite o valor do raio: ");
         double raio = scanner.nextDouble();
         
+        // Área
+        double area = Math.PI * Math.pow(raio, 2);
         
-        double area = pi * Math.pow(raio, 2);
-        
-        
-        System.out.printf("A área do círculo é: %.4f\n", area);
-        
-       scanner.close();
-    
+        // Resultado
+        System.out.printf("A área do círculo é: %.4f%n", area);
+
+        // Scanner
+        scanner.close();
     }
 }
